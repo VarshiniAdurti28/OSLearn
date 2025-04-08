de87b92cbe1324731488bb648df4e5e657ae857a
Aalima201 <143832900+Aalima201@users.noreply.github.com>
2025-04-09 00:52:13 +0530

    Update bankers.js

--- app/static/js/deadlock/bankers.js
+++ app/static/js/deadlock/bankers.js
@@ -1,267 +1,115 @@
-var res, pro;
-var resource;
-function BuildFormFields($amount) // For Resorce allocation
-{
+function BuildFormFields(n) {
+    const container = document.getElementById("FormFields");
+    container.innerHTML = "";
+    for (let i = 0; i < n; i++) {
+        container.innerHTML += `<label>Resource ${i}</label><input type="number" class="form-control" id="res${i}" /><br/>`;
+    }
+}
 
-	res = $amount;
-	if (res <= 0)
-		alert("Invalid Inputs");
-	var
-		$container = document.getElementById('FormFields'),
-		$item, $field, $i;
-	$container.innerHTML = '';
-	for ($i = 0; $i < $amount; $i++) {
-		$item = document.createElement('div');
-		$item.style.margin = '10px';
+function BuildFormFields2(p) {
+    const r = document.querySelectorAll("#FormFields input").length;
+    const needContainer = document.getElementById("FormFields1");
+    const allocContainer = document.getElementById("FormFields2");
+    needContainer.innerHTML = "";
+    allocContainer.innerHTML = "";
 
-		$field = document.createElement('label');
-		$field.innerHTML = 'Resource ' + $i;
-		$item.appendChild($field);
-		$field = document.createElement('input');
-		$field.name = 'Design[' + $i + ']';
-		$field.type = 'text';
-		$field.setAttribute("class", "form-control");
-		$item.appendChild($field);
-		$container.appendChild($item);
-	}
+    for (let i = 0; i < p; i++) {
+        needContainer.innerHTML += `<strong>Process ${i}</strong><br/>`;
+        allocContainer.innerHTML += `<strong>Process ${i}</strong><br/>`;
+
+        for (let j = 0; j < r; j++) {
+            needContainer.innerHTML += `Resource ${j}: <input type="number" id="need_${i}_${j}" /><br/>`;
+            allocContainer.innerHTML += `Resource ${j}: <input type="number" id="alloc_${i}_${j}" /><br/>`;
+        }
+    }
 }
-function BuildFormFields2($amount) //For Allocation
-{
-	pro = $amount;
-	if ($amount <= 0)
-		alert("Invalid Inputs");
-	console.log(res);
-	var
-		$container = document.getElementById('FormFields1'),
-		$item, $field, $i;
-	$container.innerHTML = '';
-	$item = document.createElement('h4');
-	$item.innerHTML = 'Need';
-	$container.appendChild($item);
-	for ($i = 0; $i < $amount; $i++) {
-		$item = document.createElement('h5');
-		$item.innerHTML = 'Process ' + $i;
-		$container.appendChild($item);
-		for ($j = 0; $j < res; $j++) {
-			$item = document.createElement('div');
-			$item.style.margin = '10px';
-			$field = document.createElement('label');
-			$field.innerHTML = 'Resource ' + $j + ':';
-			$item.appendChild($field);
-			$field = document.createElement('input');
-			$field.name = 'Design[' + $i + ']';
-			$field.type = 'text';
-			$field.setAttribute("class", "form-control");
-			$item.appendChild($field);
-			$container.appendChild($item);
-		}
-	}
-	BuildFormFields3($amount);
-}
-function BuildFormFields3($amount) //For Allocation
-{
-	if ($amount <= 0)
-		alert("Invalid Inputs");
-	console.log(res);
-	var
-		$container = document.getElementById('FormFields2'),
-		$item, $field, $i;
-	$container.innerHTML = '';
-	$item = document.createElement('h4');
-	$item.innerHTML = 'Allocated';
-	$container.appendChild($item);
-	for ($i = 0; $i < $amount; $i++) {
-		$item = document.createElement('h5');
-		$item.innerHTML = 'Process ' + $i;
-		$container.appendChild($item);
-		for ($j = 0; $j < res; $j++) {
-			$item = document.createElement('div');
-			$item.style.margin = '10px';
-			$field = document.createElement('label');
-			$field.innerHTML = 'Resource ' + $j + ':';
-			$item.appendChild($field);
-			$field = document.createElement('input');
-			$field.name = 'Design[' + $i + ']';
-			$field.type = 'text';
-			$field.setAttribute("class", "form-control");
-			$item.appendChild($field);
-			$container.appendChild($item);
-		}
-	}
-}
-var y;
+
 function banker() {
-	var res_form = document.resource;
-	var resource = [];
-	for (var i = 1; i <= res; i++) {
-		resource[i - 1] = Number(res_form[i].value);
-	}
-	console.log(resource);
-	var form = document.Need;
-	var ned = [];
-	var i, j;
-	console.log("PRO: " + pro + "RES: " + res);
-	for (i = 1; i <= pro * res; i++) {
-		ned[i - 1] = Number(form[i].value);
-	}
-	var needy = [];
-	for (var i = 0; i < pro; i++) {
-		needy[i] = [];
-		for (var j = 0; j < res; j++) {
-			needy[i][j] = ned[i * res + j];
-		}
-	}
-	console.log(ned);
-	console.log(needy);
-	var alloc_form = document.Alloc;
-	var alloc = [];
-	for (var i = 0; i < pro; i++) {
-		alloc[i] = [];
-		for (var j = 0; j < res; j++) {
-			alloc[i][j] = Number(alloc_form[i * res + j].value);
-			console.log(alloc[i][j]);
-		}
-	}
-	console.log(alloc);
-	var avail = [];
-	for (var i = 0; i < res; i++)
-		avail[i] = resource[i];
-	for (var i = 0; i < pro; i++) {
-		for (var j = 0; j < res; j++) {
-			avail[j] = avail[j] - alloc[i][j];
-			needy[i][j] = needy[i][j] - alloc[i][j];
-		}
-	}
-	console.log('avail: ' + avail);
-	//TABLE for NEED
-	$('#tab_need tr').remove();
-	var tab_need = document.getElementById("tab_need");
-	var row = tab_need.insertRow(0);
-	var cell = row.insertCell(0);
-	cell.innerHTML = "<b>Need</b>";
-	var pro_head = tab_need.insertRow(1);
-	var cell = pro_head.insertCell(0);
-	cell.innerHTML = "<b>Process</b>";
-	for (i = 1; i <= res; i++) {
-		cell = pro_head.insertCell(i);
-		cell.innerHTML = "<b>Res " + (i - 1) + "</b>";
-	}
-	for (i = 0; i < pro; i++) {
-		var row = tab_need.insertRow(i + 2);
-		var cell = row.insertCell(0);
-		cell.innerHTML = "<b>P" + i + "</b>";
-		for (j = 0; j < res; j++) {
-			var cell = row.insertCell(j + 1);
-			cell.innerHTML = ned[res * i + j];
-		}
-	}
-	//TABLE for ALLOC
-	$('#tab_alloc tr').remove();
-	var tab_alloc = document.getElementById("tab_alloc");
-	var row = tab_alloc.insertRow(0);
-	var cell = row.insertCell(0);
-	cell.innerHTML = "<b>Allocation</b>";
-	var alloc_head = tab_alloc.insertRow(1);
-	var cell = alloc_head.insertCell(0);
-	cell.innerHTML = "<b>Process</b>";
-	for (i = 1; i <= res; i++) {
-		cell = alloc_head.insertCell(i);
-		cell.innerHTML = "<b>Res " + (i - 1) + "</b>";
-	}
-	for (i = 0; i < pro; i++) {
-		var row = tab_alloc.insertRow(i + 2);
-		var cell = row.insertCell(0);
-		cell.innerHTML = "<b>P" + i + "</b>";
-		for (j = 0; j < res; j++) {
-			var cell = row.insertCell(j + 1);
-			cell.innerHTML = alloc[i][j];
-		}
-	}
-	var prints = document.getElementById("printing");
-	prints.innerHTML = '';
-	var i, j;
-	var exec = 0; //Number of executed processes
-	var ex = [0, 0];
-	for (var i = 0; i < pro; i++) {
-		ex[i] = 0;
-	}
-	var iter = 1;
-	while (1) {
-		var exec1 = exec // exec1 is compared with exec at the end of the while loop to check for deadlock
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode("Iteration " + iter));
-		prints.appendChild(li);
-		var ul1 = document.createElement("ul");
-		for (i = 0; i < pro; i++) {
-			var flag = 0
-			if (ex[i] == 0) {
-				var li = document.createElement("li");
-				li.appendChild(document.createTextNode("Process " + i + ": "));
-				ul1.append(li);
-				prints.appendChild(ul1);
-				var ul2 = document.createElement("ul");
-				for (var j = 0; j < res; j++) {
-					if ((avail[j]) < needy[i][j]) {
-						flag = 1;
-						break;
-					}
-				}
-				if (flag) {
-					console.log("Process " + i + " doesn't have enough resources");
-					console.log("Need: " + needy[i]);
-					console.log("Current resources available: " + avail);
-					var li = document.createElement("li");
-					li.appendChild(document.createTextNode("Process " + i + " doesn't have enough resources"));
-					ul2.append(li);
-					ul1.append(ul2);
-					prints.appendChild(ul1);
-					var li = document.createElement("li");
-					li.appendChild(document.createTextNode("Need: " + needy[i]));
-					ul2.append(li);
-					ul1.append(ul2);
-					prints.appendChild(ul1);
-					var li = document.createElement("li");
-					li.appendChild(document.createTextNode("Avail: " + avail));
-					ul2.append(li);
-					ul1.append(ul2);
-					prints.appendChild(ul1);
-				}
-				else {
-					exec = exec + 1
-					console.log("Process " + i + " has enough resources");
-					var li = document.createElement("li");
-					li.appendChild(document.createTextNode("Process " + i + " has enough resources"));
-					ul2.append(li);
-					ul1.append(ul2);
-					prints.appendChild(ul1);
-					ex[i] = 1;
-					for (var j = 0; j < res; j++) {
-						avail[j] = avail[j] + alloc[i][j];
-					}
-					console.log("Current resources available: " + avail);
-					var li = document.createElement("li");
-					li.appendChild(document.createTextNode("Avail: " + avail));
-					ul2.append(li);
-					ul1.append(ul2);
-					prints.appendChild(ul1);
-				}
-			}
-		}
-		iter++;
-		if (exec1 == exec && exec != pro) // Checks for deadlock
-		{
-			console.log("Deadlock");
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Deadlock"));
-			prints.appendChild(li);
-			break;
-		}
-		if (exec == pro) {
-			console.log("Safe State");
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Safe State"));
-			prints.appendChild(li);
-			break;
-		}
-	}
+    const r = document.querySelectorAll("#FormFields input").length;
+    const p = document.querySelectorAll("#FormFields1 input").length / r;
+
+    const available = [];
+    for (let i = 0; i < r; i++) {
+        available.push(parseInt(document.getElementById(`res${i}`).value, 10));
+    }
+
+    const need = Array.from({ length: p }, () => Array(r).fill(0));
+    const alloc = Array.from({ length: p }, () => Array(r).fill(0));
+    const max = Array.from({ length: p }, () => Array(r).fill(0));
+
+    for (let i = 0; i < p; i++) {
+        for (let j = 0; j < r; j++) {
+            const needVal = parseInt(document.getElementById(`need_${i}_${j}`).value, 10);
+            const allocVal = parseInt(document.getElementById(`alloc_${i}_${j}`).value, 10);
+            need[i][j] = needVal;
+            alloc[i][j] = allocVal;
+            max[i][j] = needVal + allocVal;
+        }
+    }
+
+    // Update Need Matrix Table
+    let needHTML = "<tr><th>Process</th>";
+    for (let j = 0; j < r; j++) {
+        needHTML += `<th>R${j}</th>`;
+    }
+    needHTML += "</tr>";
+    for (let i = 0; i < p; i++) {
+        needHTML += `<tr><td>P${i}</td>`;
+        for (let j = 0; j < r; j++) {
+            needHTML += `<td>${need[i][j]}</td>`;
+        }
+        needHTML += "</tr>";
+    }
+    document.getElementById("tab_need").innerHTML = needHTML;
+
+    // Update Allocation Matrix Table
+    let allocHTML = "<tr><th>Process</th>";
+    for (let j = 0; j < r; j++) {
+        allocHTML += `<th>R${j}</th>`;
+    }
+    allocHTML += "</tr>";
+    for (let i = 0; i < p; i++) {
+        allocHTML += `<tr><td>P${i}</td>`;
+        for (let j = 0; j < r; j++) {
+            allocHTML += `<td>${alloc[i][j]}</td>`;
+        }
+        allocHTML += "</tr>";
+    }
+    document.getElementById("tab_alloc").innerHTML = allocHTML;
+
+    // Safety Algorithm
+    const finish = Array(p).fill(false);
+    const work = [...available];
+    const safeSequence = [];
+
+    let madeProgress;
+    do {
+        madeProgress = false;
+        for (let i = 0; i < p; i++) {
+            if (!finish[i]) {
+                let canProceed = true;
+                for (let j = 0; j < r; j++) {
+                    if (need[i][j] > work[j]) {
+                        canProceed = false;
+                        break;
+                    }
+                }
+                if (canProceed) {
+                    for (let j = 0; j < r; j++) {
+                        work[j] += alloc[i][j];
+                    }
+                    finish[i] = true;
+                    safeSequence.push(`P${i}`);
+                    madeProgress = true;
+                }
+            }
+        }
+    } while (madeProgress);
+
+    const isSafe = finish.every(f => f === true);
+    let resultText = isSafe
+        ? `<p>System is in a <strong>SAFE</strong> state.</p><p>Safe sequence: ${safeSequence.join(" → ")}</p>`
+        : `<p style="color: red;">System is <strong>NOT SAFE</strong>. Deadlock may occur.</p>`;
+
+    showResults(resultText, isSafe);
 }

1173eb82d201a9aa0d92382a2940eb041c0451f1
Abhirams144 <147538229+Abhirams144@users.noreply.github.com>
2025-04-14 02:59:13 +0530

    Update sstf.js

--- app/static/js/disk_scheduling/sstf.js
+++ app/static/js/disk_scheduling/sstf.js
@@ -1,106 +1,96 @@
 import { getReferenceString } from "./ds_base.js";
 
 $(document).ready(function () {
-    // var dp_fcfs = [];
-    var dp_sstf = [];
-    // var dp_scan = [];
-    // var dp_scan = [];
-    // var dp_look = [];
-    // var dp_clook = [];
-    // var fcfs_values = [];
-    var sstf_values = [];
-    // var scan_values = [];
-    // var scan_values=[];
-    // var look_values = [];
-    // var clook_values = [];
-    var data_sstf = {};
+  let dp_sstf = [];
+  let sstf_values = [];
+  let data_sstf = {};
 
-    $("#calc").click(function calculate() {
-        sstf_values = [];
-        dp_sstf = [];
-        data_sstf = [];
-        var i;
-        var sum = 0;
-        var diff;
+  $("#calc").click(function calculateSSTF() {
+    sstf_values = [];
+    dp_sstf = [];
+    data_sstf = [];
+    let totalMovement = 0;
 
-        var head = parseInt(document.getElementsByTagName("input")[0].value);
-        var max = parseInt(document.getElementsByTagName("input")[2].value);
-        var min = parseInt(document.getElementsByTagName("input")[1].value);
-        if (head > max) {
-            alert("fcfs head is larger than last cylinder number");
-            return -10;
-        }
+    // Get user inputs
+    const head = parseInt(document.getElementsByTagName("input")[0].value);
+    const min = parseInt(document.getElementsByTagName("input")[1].value);
+    const max = parseInt(document.getElementsByTagName("input")[2].value);
 
-        if (head < min) {
-            alert("fcfs head is smaller than first cylinder number");
-            return -10;
-        }
+    if (head > max || head < min) {
+      alert("The head position must be within the range of cylinders.");
+      return;
+    }
 
-        sstf_values.push(head);
-        var in_arr = getReferenceString(head, max, min);
+    // Get reference string
+    sstf_values.push(head);
+    const in_arr = getReferenceString(head, max, min);
+    if (!in_arr.length) {
+      alert("Reference string is empty! Please provide valid inputs.");
+      return;
+    }
 
-        var min = 9999;
-        var done = [];		//a new empty array
-        var flag;
-        //make done as 0
-        for (i = 0; i < in_arr.length - 2; ++i)		//-2 because there are 2 checkboxes , do -8 if min and max are included
-            done.push(0);
+    // SSTF Algorithm
+    let current = head;
+    const requests = [...in_arr];
+    while (requests.length > 0) {
+      // Find the closest request to the current position
+      const nearest = requests.reduce((closest, request) =>
+        Math.abs(request - current) < Math.abs(closest - current) ? request : closest
+      );
 
+      // Calculate movement and update current position
+      totalMovement += Math.abs(nearest - current);
+      current = nearest;
 
-        for (var j = 1; j <= in_arr.length - 2; ++j) {
-            for (var i = 1; i <= in_arr.length - 2; ++i) {
-                diff = head - in_arr[i];
+      // Update visited sequence
+      sstf_values.push(nearest);
 
-                if (diff < 0)
-                    diff = diff * (-1);
+      // Remove the processed request
+      requests.splice(requests.indexOf(nearest), 1);
+    }
 
-                if ((diff < min) && (done[i - 1] == 0)) {
-                    min = diff;
-                    flag = i;
-                    //here we push the variables for the graph
+    allocateSSTF();
+    alert(`Total head movement with SSTF: ${totalMovement}`);
+    return totalMovement;
+  });
 
-                }
-            }
-            sum = sum + min;
-            head = in_arr[flag];
-            //dp_sstf.push([in_arr[flag],y_i]);
-            sstf_values.push(in_arr[flag]);
-            done[flag - 1] = 1;
-            min = 9999;
-        }
-        allocate_sstf();
-        return sum;
+  function allocateSSTF() {
+    sstf_values.forEach((value, index) => {
+      dp_sstf.push([value, index]);
     });
 
-    function allocate_sstf() {
-        var d = -1;
-        for (var i = 0; i < sstf_values.length; ++i) {
-            d++;
-            var a = parseInt(sstf_values[i]);
-            dp_sstf.push([a, d]);
-        }
-        data_sstf = {
-            values: dp_sstf,
-        };
-        sstfModal();
-    }
+    data_sstf = {
+      values: dp_sstf
+    };
 
-    function sstfModal() {
-        var max_val = document.getElementsByTagName("input")[2].value;
-        zingchart.render({
-            id: "chartContainer",
-            output: "svg",
-            height: 500,
-            width: "80%",
-            data: {
-                "type": "line",
-                "title": {
-                    "text": "SSTF Header Movement"
-                },
-                "series": [
-                    data_sstf
-                ]
-            }
-        });
-    }
+    renderSSTFChart();
+  }
+
+  function renderSSTFChart() {
+    zingchart.render({
+      id: "chartContainer",
+      output: "svg",
+      height: 500,
+      width: "80%",
+      data: {
+        type: "line",
+        title: {
+          text: "SSTF Head Movement Visualization"
+        },
+        series: [data_sstf]
+      },
+      "scale-x": {
+        title: {
+          text: "Request Track Number"
+        }
+      },
+      "scale-y": {
+        title: {
+          text: "Head Movement (Distance)"
+        }
+      }
+    });
+
+    $("#chartContainer").removeClass("hidden");
+  }
 });
